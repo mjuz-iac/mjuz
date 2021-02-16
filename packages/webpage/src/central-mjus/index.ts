3664644de310d3df8f7b973a06ac94a00c8b489a
Daniel Sokolowski <github@d.sokolowski.org>
2021-02-16 19:32:23 +0100

    Completed central  webpage example
    Including reactive changes

--- packages/webpage/src/central-mjus/index.ts
+++ packages/webpage/src/central-mjus/index.ts
@@ -9,21 +9,99 @@ import {
 	sigint,
 	sigterm,
 } from '@mjus/core';
-import { empty } from '@funkia/hareactive';
+import { at, changes, sinkBehavior } from '@funkia/hareactive';
 import { runIO } from '@funkia/io';
+import * as aws from '@pulumi/aws';
+import { PulumiFn } from '@pulumi/pulumi/x/automation';
+import { isDeepStrictEqual } from 'util';
 
 const logger = newLogger('deployment');
 
-const program = emptyProgram;
+type State = { counter: number };
+const programState = sinkBehavior<State>({ counter: 0 });
+// Counter changes every 20 seconds, even though it is updated every second
+setInterval(
+	() =>
+		programState.push({
+			counter: at(programState).counter + (Date.now() % 20000 < 1000 ? 1 : 0),
+		}),
+	1000
+);
 
-const initStack = () =>
-	getStack({
-		program: program,
-		projectName: 'CentralizedWebPage',
-		stackName: 'CentralizedWebPage',
+const program = (state: State): PulumiFn => async () => {
+	/**
+	 * Based on inline program ts pulumi automation API example: https://github.com/pulumi/automation-api-examples/tree/main/nodejs/inlineProgram-ts
+	 */
+
+	// Create a bucket and expose a website index document
+	const bucket = new aws.s3.Bucket('website', {
+		website: {
+			indexDocument: 'index.html',
+		},
 	});
 
-const deployment = loop(initStack, operations(program), nextAction(empty, sigint, sigterm));
+	const content = `<html>
+		<head>
+			<title>Hello World! 👋🌍</title>
+			<meta charset="UTF-8">
+		</head>
+		<body>
+			<p>Hello World! 👋🌍</p>
+			<p>Counter: ${state.counter}</p>
+		</body>
+	</html>`;
+
+	// write our index.html into the site bucket
+	new aws.s3.BucketObject('index', {
+		bucket: bucket,
+		content: content,
+		contentType: 'text/html; charset=utf-8',
+		key: 'index.html',
+	});
+
+	// Configure bucket policy to allow public access
+	function publicReadPolicyForBucket(bucketArn: string): aws.iam.PolicyDocument {
+		return {
+			Version: '2012-10-17',
+			Statement: [
+				{
+					Effect: 'Allow',
+					Principal: '*',
+					Action: ['s3:GetObject'],
+					Resource: [`${bucketArn}/*`],
+				},
+			],
+		};
+	}
+
+	// Register bucket policy
+	new aws.s3.BucketPolicy('bucket-contents-policy', {
+		bucket: bucket.bucket,
+		policy: bucket.arn.apply(publicReadPolicyForBucket),
+	});
+
+	// Export the Internet address for the service.
+	return {
+		url: bucket.websiteEndpoint,
+	};
+};
+
+const initStack = () =>
+	getStack(
+		{
+			program: emptyProgram,
+			projectName: 'CentralizedWebPage',
+			stackName: 'CentralizedWebPage',
+		},
+		undefined,
+		{ 'aws:region': { value: 'us-east-1' } }
+	);
+
+const deployment = loop(
+	initStack,
+	operations(programState.map(program)),
+	nextAction(changes(programState, isDeepStrictEqual), sigint(), sigterm())
+);
 
 runIO(deployment)
 	.catch((err) => {
