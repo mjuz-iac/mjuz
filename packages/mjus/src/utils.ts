04e45f49de33fc93e319818dc31c71ff991ccd8d
Daniel Sokolowski <github@d.sokolowski.org>
2021-02-16 19:00:04 +0100

    Handle SIGINT and SIGTERM interrupts only on demand

--- packages/mjus/src/utils.ts
+++ packages/mjus/src/utils.ts
@@ -1,11 +1,17 @@
 import { Future, sinkFuture } from '@funkia/hareactive';
 import Timeout = NodeJS.Timeout;
 
-export const sigint: Future<void> = sinkFuture();
-process.on('SIGINT', () => sigint.resolve());
+export const sigint: () => Future<void> = () => {
+	const f = sinkFuture<void>();
+	process.on('SIGINT', () => f.resolve());
+	return f;
+};
 
-export const sigterm: Future<void> = sinkFuture();
-process.on('SIGTERM', () => sigterm.resolve());
+export const sigterm: () => Future<void> = () => {
+	const f = sinkFuture<void>();
+	process.on('SIGTERM', () => f.resolve());
+	return f;
+};
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const keepAlive = (): Timeout => setInterval(() => {}, 2147483647);
