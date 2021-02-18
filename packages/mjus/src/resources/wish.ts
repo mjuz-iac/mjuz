ff474d7538c8a6ce2e7dd42343c5443ecf15d746
Daniel Sokolowski <github@d.sokolowski.org>
2021-02-18 16:25:59 +0100

    Wish deployment working with static dummy value

--- packages/mjus/src/resources/wish.ts
+++ packages/mjus/src/resources/wish.ts
@@ -2,25 +2,23 @@ import { CustomResourceOptions, dynamic, Input, Output } from '@pulumi/pulumi';
 import { WrappedInputs, WrappedOutputs } from '../type-utils';
 import { RemoteConnection } from '..';
 
-type WishInputs = {
+type WishInputs<O> = {
 	offerName: string;
 	target: RemoteConnection;
-};
-type WishOutputs<O> = WishInputs & {
 	value: O;
 };
 
 class WishProvider<O> implements dynamic.ResourceProvider {
 	async create(
-		inputs: WishInputs // Due to serialization all `Resource` values reduced to their id
-	): Promise<dynamic.CreateResult & { outs: WishOutputs<O> }> {
-		const offer: unknown = 'offer';
+		inputs: WishInputs<O> // Due to serialization all `Resource` values reduced to their id
+	): Promise<dynamic.CreateResult & { outs: WishInputs<O> }> {
+		const offer: unknown = 'website-40eae63';
 		return { id: `${inputs.target}:${inputs.offerName}`, outs: { ...inputs, value: <O>offer } };
 	}
 }
 
-export type WishArgs = WrappedInputs<WishInputs>;
-type WishProps<O> = Readonly<WrappedOutputs<WishOutputs<O>>>;
+export type WishArgs = WrappedInputs<Omit<WishInputs<unknown>, 'value'>>;
+type WishProps<O> = Readonly<WrappedOutputs<WishInputs<O>>>;
 export class Wish<O> extends dynamic.Resource implements WishProps<O> {
 	constructor(target: Input<RemoteConnection>, offerName: string, opts?: CustomResourceOptions);
 	constructor(name: string, props: WishArgs, opts?: CustomResourceOptions);
@@ -29,19 +27,21 @@ export class Wish<O> extends dynamic.Resource implements WishProps<O> {
 		argsOrOfferName: WishArgs | string,
 		opts?: CustomResourceOptions
 	) {
-		if (typeof nameOrTarget === 'string' && typeof argsOrOfferName !== 'string')
-			super(new WishProvider<O>(), nameOrTarget, argsOrOfferName, opts);
-		else if (typeof nameOrTarget !== 'string' && typeof argsOrOfferName === 'string')
-			super(
-				new WishProvider<O>(),
-				`${(<RemoteConnection>nameOrTarget).name}:${argsOrOfferName}`,
-				{
-					offerName: argsOrOfferName,
-					target: nameOrTarget,
-				},
-				opts
-			);
-		else throw new Error('Unsupported wish configuration');
+		const [name, args]: [string, WrappedInputs<WishInputs<null>> | undefined] =
+			typeof nameOrTarget === 'string' && typeof argsOrOfferName !== 'string'
+				? [nameOrTarget, { ...argsOrOfferName, value: null }]
+				: nameOrTarget !== 'string' && typeof argsOrOfferName === 'string'
+				? [
+						`${(<RemoteConnection>nameOrTarget).name}:${argsOrOfferName}`,
+						{
+							offerName: argsOrOfferName,
+							target: <Input<RemoteConnection>>nameOrTarget,
+							value: null,
+						},
+				  ]
+				: ['invalid wish configuration', undefined];
+		if (!args) throw new Error('Unsupported wish configuration');
+		super(new WishProvider<O>(), name, args, opts);
 	}
 
 	public readonly offerName!: Output<string>;
