eec35dbadae2cb47a9e085a2dce780896bd8f5b3
Daniel Sokolowski <github@d.sokolowski.org>
2021-02-18 16:39:40 +0100

    Improved offer

--- packages/mjus/src/resources/offer.ts
+++ packages/mjus/src/resources/offer.ts
@@ -6,18 +6,17 @@ type OfferInputs<O> = {
 	offerName: string;
 	offer: O;
 };
-type OfferOutputs<O> = OfferInputs<O>;
 
 class OfferProvider<O> implements dynamic.ResourceProvider {
 	async create(
 		inputs: OfferInputs<O> // Due to serialization all `Resource` values reduced to their id
-	): Promise<dynamic.CreateResult & { outs: OfferOutputs<O> }> {
+	): Promise<dynamic.CreateResult & { outs: OfferInputs<O> }> {
 		return { id: `${inputs.beneficiary}:${inputs.offerName}`, outs: inputs };
 	}
 }
 
 type OfferArgs<O> = WrappedInputs<OfferInputs<O>>;
-type OfferProps<O> = Readonly<WrappedOutputs<OfferOutputs<O>>>;
+type OfferProps<O> = Readonly<WrappedOutputs<OfferInputs<O>>>;
 export class Offer<O> extends dynamic.Resource implements OfferProps<O> {
 	constructor(
 		beneficiary: Input<RemoteConnection>,
@@ -32,25 +31,26 @@ export class Offer<O> extends dynamic.Resource implements OfferProps<O> {
 		optsOrOffer: CustomResourceOptions | Input<O>,
 		opts?: CustomResourceOptions
 	) {
-		if (typeof nameOrBeneficiary === 'string' && typeof argsOrOfferName !== 'string')
-			super(
-				new OfferProvider<O>(),
-				nameOrBeneficiary,
-				argsOrOfferName,
-				<CustomResourceOptions>optsOrOffer
-			);
-		else if (typeof nameOrBeneficiary !== 'string' && typeof argsOrOfferName === 'string')
-			super(
-				new OfferProvider<O>(),
-				`${(<RemoteConnection>nameOrBeneficiary).name}:${argsOrOfferName}`,
-				{
-					beneficiary: nameOrBeneficiary,
-					offerName: argsOrOfferName,
-					offer: optsOrOffer,
-				},
-				opts
-			);
-		else throw new Error('Unsupported offer configuration');
+		const [name, args, opt]: [
+			string,
+			OfferArgs<O> | undefined,
+			CustomResourceOptions | undefined
+		] =
+			typeof nameOrBeneficiary === 'string' && typeof argsOrOfferName !== 'string'
+				? [nameOrBeneficiary, argsOrOfferName, <CustomResourceOptions>optsOrOffer]
+				: typeof nameOrBeneficiary !== 'string' && typeof argsOrOfferName === 'string'
+				? [
+						`${(<RemoteConnection>nameOrBeneficiary).name}:${argsOrOfferName}`,
+						{
+							beneficiary: nameOrBeneficiary,
+							offerName: argsOrOfferName,
+							offer: <Input<O>>optsOrOffer,
+						},
+						opts,
+				  ]
+				: ['invalid offer configuration', undefined, undefined];
+		if (!args) throw new Error('Unsupported offer configuration');
+		super(new OfferProvider<O>(), name, args, opt);
 	}
 
 	public readonly beneficiary!: Output<RemoteConnection>;
