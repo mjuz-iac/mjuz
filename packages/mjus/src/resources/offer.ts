843ee91bb4aee5d26ceae00a973a7b1daa782be7
Daniel Sokolowski <github@d.sokolowski.org>
2021-02-20 13:46:43 +0100

    Implemented offer diff and update operations

--- packages/mjus/src/resources/offer.ts
+++ packages/mjus/src/resources/offer.ts
@@ -4,8 +4,9 @@ import * as rpc from '@mjus/grpc-protos';
 import { deleteOffer, updateOffer } from '../runtime-offers';
 import { RemoteConnection } from './remote-connection';
 import { WrappedInputs, WrappedOutputs } from '../type-utils';
+import { isDeepStrictEqual } from 'util';
 
-type OfferInputs<O> = {
+type OfferProps<O> = {
 	beneficiary: RemoteConnection;
 	offerName: string;
 	offer: O;
@@ -17,8 +18,8 @@ class OfferProvider<O> implements dynamic.ResourceProvider {
 	// the deployment did not run through. For now: make sure this function won't reject. For debugging, we use an error
 	// input property.
 	async create(
-		inputs: OfferInputs<O> // Due to serialization all `Resource` values reduced to their id
-	): Promise<dynamic.CreateResult & { outs: OfferInputs<O> }> {
+		inputs: OfferProps<O> // Due to serialization all `Resource` values reduced to their id
+	): Promise<dynamic.CreateResult & { outs: OfferProps<O> }> {
 		try {
 			const offer = new rpc.Offer()
 				.setName(inputs.offerName)
@@ -26,7 +27,11 @@ class OfferProvider<O> implements dynamic.ResourceProvider {
 				.setOffer(Value.fromJavaScript(inputs.offer || null));
 			await updateOffer(offer);
 
-			return { id: `${inputs.beneficiary}:${inputs.offerName}`, outs: inputs };
+			const outProps: OfferProps<O> = {
+				...inputs,
+				error: null,
+			};
+			return { id: `${inputs.beneficiary}:${inputs.offerName}`, outs: outProps };
 		} catch (e) {
 			return {
 				id: `${inputs.beneficiary}:${inputs.offerName}`,
@@ -35,7 +40,49 @@ class OfferProvider<O> implements dynamic.ResourceProvider {
 		}
 	}
 
-	async delete(id: ID, inputs: OfferInputs<O>): Promise<void> {
+	async diff(
+		id: ID,
+		oldProps: OfferProps<O>,
+		newProps: OfferProps<O>
+	): Promise<dynamic.DiffResult> {
+		const targetChanged =
+			oldProps.beneficiary !== newProps.beneficiary ||
+			oldProps.offerName !== newProps.offerName;
+		const offerChanged = targetChanged || !isDeepStrictEqual(oldProps.offer, newProps.offer);
+
+		return {
+			changes: offerChanged,
+			replaces: [
+				oldProps.beneficiary !== newProps.beneficiary ? 'beneficiary' : null,
+				oldProps.offerName !== newProps.offerName ? 'offerName' : null,
+			].filter((v) => v !== null) as string[],
+			deleteBeforeReplace: true,
+		};
+	}
+
+	async update(
+		id: ID,
+		oldProps: OfferProps<O>,
+		newProps: OfferProps<O>
+	): Promise<dynamic.UpdateResult> {
+		try {
+			const offer = new rpc.Offer()
+				.setName(newProps.offerName)
+				.setBeneficiaryid(`${newProps.beneficiary}`)
+				.setOffer(Value.fromJavaScript(newProps.offer || null));
+			await updateOffer(offer);
+
+			const outProps: OfferProps<O> = {
+				...newProps,
+				error: null,
+			};
+			return { outs: outProps };
+		} catch (e) {
+			return { outs: { ...oldProps, error: e.message } };
+		}
+	}
+
+	async delete(id: ID, inputs: OfferProps<O>): Promise<void> {
 		const offer = new rpc.Offer()
 			.setName(inputs.offerName)
 			.setBeneficiaryid(`${inputs.beneficiary}`);
@@ -43,9 +90,9 @@ class OfferProvider<O> implements dynamic.ResourceProvider {
 	}
 }
 
-type OfferArgs<O> = WrappedInputs<Omit<OfferInputs<O>, 'error'>>;
-type OfferProps<O> = Readonly<WrappedOutputs<OfferInputs<O>>>;
-export class Offer<O> extends dynamic.Resource implements OfferProps<O> {
+type OfferArgs<O> = WrappedInputs<Omit<OfferProps<O>, 'error'>>;
+type OfferOutputs<O> = Readonly<WrappedOutputs<OfferProps<O>>>;
+export class Offer<O> extends dynamic.Resource implements OfferOutputs<O> {
 	constructor(
 		beneficiary: Input<RemoteConnection>,
 		offerName: string,
@@ -78,7 +125,11 @@ export class Offer<O> extends dynamic.Resource implements OfferProps<O> {
 				  ]
 				: ['invalid offer configuration', undefined, undefined];
 		if (!args) throw new Error('Unsupported offer configuration');
-		super(new OfferProvider<O>(), name, { ...args, error: null }, opt);
+		const props: WrappedInputs<OfferProps<O>> = {
+			...args,
+			error: null,
+		};
+		super(new OfferProvider<O>(), name, props, opt);
 	}
 
 	public readonly beneficiary!: Output<RemoteConnection>;
