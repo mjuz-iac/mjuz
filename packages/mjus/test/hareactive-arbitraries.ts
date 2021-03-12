6f98a814b456951729f4923fcd8d8800ca4db67c
Daniel Sokolowski <github@d.sokolowski.org>
2021-03-12 18:46:32 +0100

    Add hareactive fast check arbitratries

---
+++ packages/mjus/test/hareactive-arbitraries.ts
@@ -0,0 +1,63 @@
+import { Future, never, Stream } from '@funkia/hareactive';
+import { testFuture, testStreamFromArray } from '@funkia/hareactive/testing';
+import * as fc from 'fast-check';
+
+export type OccurringFutureArbConstraints = {
+	minTime?: number;
+	maxTime?: number;
+};
+export const occurringFutureArb = <T>(
+	valueArb: fc.Arbitrary<T>,
+	constraints: OccurringFutureArbConstraints = {}
+): fc.Arbitrary<Future<T>> => {
+	const time = fc.double({
+		next: true,
+		min: constraints.minTime,
+		max: constraints.maxTime,
+		noNaN: true,
+	});
+	return fc.tuple(time, valueArb).map((t) => testFuture(...t));
+};
+
+export type FutureArbConstraints = OccurringFutureArbConstraints & {
+	// Probability to return never future is 1/freq. If false, future always occurs. Defaults to 5 (20%).
+	freq?: number | false;
+};
+export const futureArb = <T>(
+	value: fc.Arbitrary<T>,
+	constraints: FutureArbConstraints = {}
+): fc.Arbitrary<Future<T>> =>
+	constraints.freq === false
+		? occurringFutureArb(value, constraints)
+		: fc.option(occurringFutureArb(value, constraints), {
+				freq: constraints.freq,
+				nil: never,
+		  });
+
+export type StreamArbConstraints = {
+	minEvents?: number;
+	maxEvents?: number;
+	minTime?: number;
+	maxTime?: number;
+};
+export const streamArb = <T>(
+	valueArb: fc.Arbitrary<T>,
+	constraints: StreamArbConstraints = {}
+): fc.Arbitrary<Stream<T>> => {
+	const eventArb = fc.tuple(
+		fc.double({
+			next: true,
+			min: constraints.minTime,
+			max: constraints.maxTime,
+			noNaN: true,
+		}),
+		valueArb
+	);
+	return fc
+		.array(eventArb, {
+			minLength: constraints.minEvents,
+			maxLength: constraints.maxEvents,
+		}) // Stream array must be sorted for correct semantics
+		.map((s) => s.sort((a, b) => a[0] - b[0]))
+		.map(testStreamFromArray);
+};
