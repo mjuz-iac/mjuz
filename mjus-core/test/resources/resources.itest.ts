22cf6ef1e013037cd146a5bd38a0e2ddac03116b
Daniel Sokolowski <github@d.sokolowski.org>
2021-04-03 15:17:20 +0200

    Improved Offer integration tests

--- mjus-core/test/resources/resources.itest.ts
+++ mjus-core/test/resources/resources.itest.ts
@@ -1,5 +1,5 @@
 import { LocalWorkspace, PulumiFn, Stack } from '@pulumi/pulumi/x/automation';
-import { RemoteConnection } from '../../src/resources';
+import { Offer, RemoteConnection } from '../../src/resources';
 import { emptyProgram, ResourcesService, startResourcesService } from '../../src';
 
 describe('resources', () => {
@@ -39,6 +39,68 @@ describe('resources', () => {
 			expect(JSON.stringify(outputs)).toBe(
 				'{"r":{"value":{"error":null,"host":"127.0.0.1","id":"testRemote","name":"testRemote","port":19952,"urn":"urn:pulumi:testStack::testProject::pulumi-nodejs:dynamic:Resource::testRemote"},"secret":false}}'
 			);
+		});
+	});
+
+	describe('offer', () => {
+		test('deploy void offers', async () => {
+			const program: PulumiFn = async () => {
+				const r = new RemoteConnection('testRemote', {});
+				const o1 = new Offer(r, 'testOffer', undefined);
+				const o2 = new Offer('directlyNamedTestOffer', {
+					beneficiary: r,
+					offerName: 'testOffer',
+					offer: undefined,
+				});
+				return { o1, o2 };
+			};
+			// Confirm withdrawals
+			resourcesService.offerWithdrawn.subscribe((p) => p[1](null));
+
+			const { outputs } = await stack.up({ program });
+			expect(JSON.stringify(outputs)).toBe(
+				'{"o1":{"value":{"beneficiary":"testRemote","error":null,"id":"testRemote:testOffer","offerName":"testOffer","urn":"' +
+					'urn:pulumi:testStack::testProject::pulumi-nodejs:dynamic:Resource::testRemote:testOffer"},"secre' +
+					't":false},"o2":{"value":{"beneficiary":"testRemote","error":null,"id":"testRemote:testOffer","offerName":"testOffer' +
+					'","urn":"urn:pulumi:testStack::testProject::pulumi-nodejs:dynamic:Resource::directlyNamedTestOff' +
+					'er"},"secret":false}}'
+			);
+		});
+
+		test('deploy and update', async () => {
+			const program = <T>(offer: T): PulumiFn => async () => {
+				const r = new RemoteConnection('testRemote', {});
+				const o = new Offer('testOfferName', {
+					beneficiary: r,
+					offerName: 'testOffer',
+					offer,
+				});
+				return { o };
+			};
+			// Confirm withdrawals
+			resourcesService.offerWithdrawn.subscribe((p) => p[1](null));
+
+			{
+				const { outputs } = await stack.up({
+					program: program({ myArray: [3.4, 'test'], isTrue: true }),
+				});
+				expect(JSON.stringify(outputs)).toBe(
+					'{"o":{"value":{"beneficiary":"testRemote","error":null,"id":"testRemote:testOffer","offer":{"isTrue":true,"myArray":[3.4,"test"]},"offerName":"testOffer","urn":"urn:pulumi:testStack::testProject::pulumi-nodejs:dynamic:Resource::testOfferName"},"secret":false}}'
+				);
+			}
+
+			{
+				const { outputs } = await stack.up({
+					program: program({
+						myArray: [1.2, 'test'],
+						isTrue: false,
+						newField: 5,
+					}),
+				});
+				expect(JSON.stringify(outputs)).toBe(
+					'{"o":{"value":{"beneficiary":"testRemote","error":null,"id":"testRemote:testOffer","offer":{"isTrue":false,"myArray":[1.2,"test"],"newField":5},"offerName":"testOffer","urn":"urn:pulumi:testStack::testProject::pulumi-nodejs:dynamic:Resource::testOfferName"},"secret":false}}'
+				);
+			}
 		});
 	});
 });
